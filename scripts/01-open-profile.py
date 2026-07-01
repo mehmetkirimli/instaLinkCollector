71f96f4ad49ce34e569decafeb940af6be1723b2
mehmetkirimli <mehmet.kirimli@vbt.com.tr>
2026-07-01 10:20:33 +0300

    fix

--- scripts/01-open-profile.py
+++ scripts/01-open-profile.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 
-PROFILE_URL = "https://www.instagram.com/goturkiye/"
+PROFILE_URL = "https://www.instagram.com/visit.dubai/"
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=50)
